b0e66ce4725c4f1d500cf043bacba63e885713f6
Tolga Uslu <tolga_uslu@live.nl>
2023-03-21 20:07:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+
+# EK 1988 final 
+
+player_0 = 'Ruud Gullit'
+player_1 =  'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = player_0 + ' ' +  str(goal_0) + ',' +  ' ' + player_1 + ' ' + str(goal_1)
+report = f"{player_0} scored in the {goal_0}nd minute\n{player_1} scored in the {goal_1}th minute"
+
+
+player = "Marco van Basten"
+x = player.find("Marco")
+first_name = player[x:5]
+y = player.find("van Basten")
+last_name = player[6:]
+last_name_len = len(last_name)
+
+
+first_name_first_letter = player[:1] 
+name_short =f"{first_name_first_letter}. {last_name}"
+
+
+chant = 'Marco! Marco! Marco! Marco! Marco!'
+
+good_chant = chant[33] != ' '
+print(good_chant) 
+
+ 
+
+
+
